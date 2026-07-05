d27eb1c8d049161fc0b8965b2a027feb78e6460a
Joonas Heinonen <mrdanglars@gmail.com>
2026-07-05 16:49:43 +0300

    EXERCISE 3: Part 3 finished and optimized parts 1 and 2

--- exercise_3/exercise_3_part_3.ino
+++ exercise_3/exercise_3_part_3.ino
@@ -9,11 +9,20 @@ void setup()
   Serial.begin(9600);
   // Set the led, pin 12 to output mode.
   pinMode(12,OUTPUT);
+  // Set the TMP, pin A0, to input mode.
+  pinMode(A0, INPUT);
   
   // Using the interrupt function to increase the index.
   attachInterrupt(
     digitalPinToInterrupt(3),
     incIndex,
+    CHANGE
+  );
+
+  // Using the interrupt function to increase the index.
+  attachInterrupt(
+    digitalPinToInterrupt(2),
+    heatValue,
     CHANGE
   );
 }
@@ -30,12 +39,33 @@ void loop()
   }
 }
 
-/**
- * Increment the index and toggle the LED state.
- */
-void incIndex() {
+void incIndex()
+{
   Serial.print("Counter: ");
   Serial.println(index);
   index += 1;
   digitalWrite(12, HIGH);
+}
+
+/**
+ * Print the heatvalue from the TMP component.
+ */
+void heatValue()
+{
+  float sum = 0.0f;
+
+  for (int i = 0; i < 50; i++)
+  {
+    sum += analogRead(A0);
+    delay(20);
+  }
+
+  float avgADC = sum / 50.0f;
+  float lt = ((5 * avgADC / 1024.0) - 0.5) * 100;
+
+  Serial.print("ADC: ");
+  Serial.print(avgADC);
+
+  Serial.print(" lt: ");
+  Serial.println(lt);
 }
