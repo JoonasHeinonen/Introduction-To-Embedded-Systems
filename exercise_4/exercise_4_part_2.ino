64a200d772c9989be02ed6fc5cc73fcea573de01
Joonas Heinonen <mrdanglars@gmail.com>
2026-08-14 15:38:52 +0300

    EXERCISE-4: Finalised the part 2

--- exercise_4/exercise_4_part_2.ino
+++ exercise_4/exercise_4_part_2.ino
@@ -8,25 +8,23 @@ void setup()
 {
   // Opens a serial port.
   Serial.begin(9600);
-  // Set the led, pin 12 to output mode.
-  pinMode(12,OUTPUT);
-  // Set the TMP, pin A0, to input mode.
-  pinMode(A0, INPUT);
-  // Set the button, pin 4, to input mode.
-  pinMode(4, INPUT);
-  
-  // Using the interrupt function to print the value.
-  attachInterrupt(
-    digitalPinToInterrupt(3),
-    printMemoryValue,
-    CHANGE
-  );
 
-  // Using the interrupt function to empty the EEPROM memory.
+  pinMode(12, OUTPUT);
+  pinMode(A0, INPUT);
+  pinMode(4, INPUT);
+
+  // Attach interrupt 2.
   attachInterrupt(
     digitalPinToInterrupt(2),
     emptyMemory,
-    CHANGE
+    FALLING
+  );
+  
+  // Attach interrupt 3.
+  attachInterrupt(
+    digitalPinToInterrupt(3),
+    printMemoryValue,
+    FALLING
   );
   
   //Print length of data to run CRC on.
@@ -42,41 +40,57 @@ void setup()
 void loop()
 {
   // The variable to read the value from pin 4.
-  int switchState = digitalRead(4);
+  int switchStateFour = digitalRead(4);
 
   // If the button is not pressed, the led is off.
-  if (switchState == LOW)
+  if (switchStateFour == LOW)
   {
-    EEPROM.write(counter, 123);
+    EEPROM.write(counter - 1, counter);
 
-    Serial.print("Writing 123 to address ");
+    Serial.print("Writing ");
+    Serial.print(counter + 1);
+    Serial.print(" to address ");
     Serial.println(counter);
+
     counter++;
 
-    delay(200);
-
-    while (digitalRead(4) == LOW)
-    {
-      // Wait until the button is released.
-    }
-
-    delay(50);
+    delayButtonPress(4);
   }
+}
+
+void delayButtonPress(int pin)
+{
+  delay(200);
+
+  while (digitalRead(pin) == LOW)
+  {
+    // Wait until the button is released.
+  }
+
+  delay(50);
 }
 
 void printMemoryValue()
 {
-  Serial.println("Printing memory value");
-  for int i = 0; i < EEPROM.length(); i++)
+  for (int i = 0; i < EEPROM.length(); i++)
   {
-    Serial.print("Address ");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(EEPROM.read(i));
+    int value = EEPROM.read(i);
+
+    if (value != 0)
+    {
+      Serial.println(value);
+    }
   }
 }
 
 void emptyMemory()
 {
-  Serial.println("Emptying EEPROM memory");
+  Serial.println("Emptying the memory...");
+
+  for (int i = 0; i < EEPROM.length(); i++)
+  {
+    EEPROM.update(i, 0);
+  }
+
+  counter = 0;
 }
